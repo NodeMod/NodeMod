180f6409d103537252ea42f2cf7441ebdbb21c94
Kheeto <70845116+Kheeto@users.noreply.github.com>
2021-10-29 22:50:06 +0200

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,11 +2,30 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-router.get('/', passport.authenticate('discord'));
+// Check if an user has logged in into discord
+function isAuthorized(req, res, next) {
+    if(req.user) {
+        res.render("alreadylogged", {
+            username: req.user.username
+        });
+    } else {
+        next();
+    }
+}
+
+router.get('/', isAuthorized, passport.authenticate('discord'));
 router.get('/redirect', passport.authenticate('discord', {
-    failureRedirect: '/forbidden'
-}), (req, res) => {
-    res.send(200);
+    failureRedirect: '/failure',
+    successRedirect: '/'
+}));
+
+router.get('/logout', (req, res) => {
+    if(req.user) {
+        res.logout();
+        res.redirect('/');
+    } else {
+        res.redirect('/');
+    }
 });
 
 module.exports = router;
