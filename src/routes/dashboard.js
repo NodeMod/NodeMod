dc04bab446a65f1d30f443f77ee3654226f13db7
Kheeto <70845116+Kheeto@users.noreply.github.com>
2021-10-29 22:11:34 +0200

    Update dashboard.js

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,38 +1,7 @@
-require('dotenv').config();
-require('./strategies/discordstrategy')
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3560;
-const session = require('express-session');
-const passport = require('passport');
-const db = require('./database/database');
-const path = require('path');
+const router = express.Router();
 
-db.then(() => console.log('Connected to Database')) // Connects to the database
-    .catch(err => console.log(err)); // Logs the error if there is one
-
-const authRoute = require('./routes/auth'); // Authorization route for the website
-const dashboardRoute = require('./routes/dashboard'); // Route for the bot dashboard
-
-app.use(session({
-    secret: 'some random secret',
-    cookie: {
-        maxAge: 60000 * 60 * 24
-    },
-    saveUninitialized: false,
-    name: 'discord-oauth2'
-}))
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../views'));
-
-// Passport
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Applies auth.js middleware route
-app.use('/auth', authRoute);
-app.use('/dashboard', dashboardRoute);
-
+// Check if an user has logged in into discord
 function isAuthorized(req, res, next) {
     if(req.user) {
         console.log("User is logged in");
@@ -44,10 +13,12 @@ function isAuthorized(req, res, next) {
     }
 }
 
-app.get('/', isAuthorized, (req, res) => {
-    res.render('home');
-})
+router.get('/', isAuthorized, (req, res) => {
+    res.send(200);
+});
 
-app.listen(PORT, () => {
-    console.log("Now listening to requests on port "+PORT);
-})
+router.get('/settings', isAuthorized, (req, res) => {
+    res.send(200);
+});
+
+module.exports = router;
