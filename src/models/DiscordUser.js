9547f807ab8604e1c32a03c37fc2f0095df29ac7
Kheeto <70845116+Kheeto@users.noreply.github.com>
2021-10-30 11:06:15 +0200

    Update DiscordUser.js

--- src/models/DiscordUser.js
+++ src/models/DiscordUser.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
     discordId: { type: String, required: true },
-    username: { type: String, required: true }
+    username: { type: String, required: true },
+    discriminator: { type: String, required: true },
+    avatar: { type: String, required: true },
 });
 
 const User = module.exports = mongoose.model('User', UserSchema);
