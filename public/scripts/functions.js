03680caf3e7aa902ba68355042c2eb656e2607b0
Kheeto <70845116+Kheeto@users.noreply.github.com>
2021-10-30 11:02:28 +0200

    Create functions.js

---
+++ public/scripts/functions.js
@@ -0,0 +1,19 @@
+function openDashboard() {
+    location.pathname = "/dashboard";
+}
+
+function openAuth() {
+    location.pathname = "/auth";
+}
+
+function openHome() {
+    location.pathname = "/";
+}
+
+function logOut() {
+    location.pathname = "/auth/logout";
+}
+
+function reload() {
+    location.reload();
+}
